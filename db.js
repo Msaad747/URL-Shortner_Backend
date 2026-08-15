86c21a48ae529570b6c2476a843e86d09d763eb2
Saad <saadhunter007@gmail.com>
2026-08-15 20:22:15 +0500

    _

--- db.js
+++ db.js
@@ -1,17 +1,16 @@
 import { Sequelize } from "sequelize";
-import "dotenv/config"
-
-
+import pg from "pg";
+import"dotenv/config";
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",
-  logging: false,
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
+    dialect: "postgres",
+    dialectModule: pg,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 
 export default sequelize;
